7db1fc1317cd85b0ee904570aa54b03054f633d7
Ahmed Masud <ahmed.masud@trustifier.com>
2015-09-07 00:06:13 -0400

    gulp wip

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,14 @@ var $ = require('gulp-load-plugins')();
 var path = require('path');
 var fs = require('fs');
 var del = require('del');
-var git = require('gulp-git');
+var Promise = require('bluebird');
+var stream = require('stream');
+var source = require('vinyl-source-stream');
 var pkg = require(path.join(__dirname, 'package.json'));
+var bwr = require(path.join(__dirname, 'bower.json'));
 var TEST_DIR=path.join(__dirname, 'bower_components/' + pkg.name);
 var DIST_DIR=path.join(__dirname, 'dist');
+
 
 gulp.task('clean', function() {
   del([TEST_DIR, DIST_DIR])
@@ -15,10 +19,45 @@ gulp.task('clean', function() {
     });
 });
 
+function saveBranch() {
+  return new Promise(function(resolve, reject) {
+    $.git.stash(function(err, result) {
+      if(err)
+        return reject(null);
+      return resolve(result.slice(3));
+    });
+  })
+}
 
-gulp.task('maintainer-clean', [ 'clean' ],  function() {
+function restoreBranch(branch) {
+}
 
+function inc(importance) {
+  return Promise.try(function() {
+    $.git.checkout('releases', function(err) {
+      if(err) throw err;
+    });
+    return gulp.src(['./package.json', './bower.json'])
+    .pipe($.bump({ type: importance }))
+    .pipe(gulp.dest('./'))
+    .pipe($.filter('package.json'))
+  });
+}
+
+gulp.task('gh:rel',  function() {
+  var branch = saveBranch();
+  branch
+    .then(inc('patch'))
+    .then(new Promise(function(resolve, reject) {
+      $.git.exec({ args: 'push --all' }, function(err, result) {
+        if(err) reject(err);
+        resolve(result);
+      });
+    }))
+    .then(restoreBranch(branch));
 });
+
+
 
 gulp.task('test', function() {
   var bower_relative_path = path.relative(
